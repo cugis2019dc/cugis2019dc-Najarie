ee3621c67b51038ff263dd179bb6312bea775a50
Najarie <53440710+Najarie@users.noreply.github.com>
2019-07-31 10:09:28 -0400

    Add files via upload

---
+++ Code Day_1.py
@@ -0,0 +1,43 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+import plotly
+dir(plotly)
+print("My name is Najarie")
+print("Hello how are you doing")
+print(5*2)
+print(5/2)
+print(5-2)
+print(5**2)
+print((8/9)*3)
+print("5*2")
+
+def multiply(a,b):
+    multiply = a*b 
+    
+    print(multiply)
+
+multiply(3,4)    
+    
+def sum(a,b,c):
+    sum = a+b+c
+    print(sum)
+sum(12,3,5)
+
+def area(a,base,height):
+    area = a*base*height
+    print(area)
+    
+area(1/2,6,3)
+
+def area(base,height):
+    area = (0.5)*base*height
+
+    print('The area of the triangle with the base of',base,'and a height of',height, 'is',area,)
+    
+area(6,3)
+    
